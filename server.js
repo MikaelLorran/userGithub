9b9d5747b08581c789eff3c0eb87789e6125fd7e
MikaelLorran <kaellorran@gmail.com>
2025-09-30 21:11:10 -0300

    Commit inicial

---
+++ server.js
@@ -0,0 +1,14 @@
+import Express from 'express'
+
+const app = Express();
+const port = 3000;
+
+
+app.get('/', (req, res)=>{
+    res.send(`<a href="https://github.com/MikaelLorran">MikaelLorran</a>`)
+})
+
+app.listen(3000, ()=>{
+    console.log("Servidor rodando em: http://localhost:"+port)
+})
+
